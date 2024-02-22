6e0263ad9e5bb846968ff596cb2c81e69d32df2f
/vrain <98318713+vrain1642002@users.noreply.github.com>
2024-02-22 17:30:45 +0700

    up

--- shophandmade/src/app/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: false,
-    // apiBaseUrl: 'http://localhost:8088/api/v1', 
-     apiBaseUrl: 'https://backendshop-production-3cd7.up.railway.app/api/v1',
-    
-    
-};
-
-  
